3749bf3edc6ba974e14f79978dff013e8557a7c2
Edric <edhernal@yahoo.com>
2023-08-15 12:18:26 -0700

    edit html and js:

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.grid-container');
+
+for (let i = 0; i < 256; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('grid-square');
+    container.appendChild(gridSquare);
+}
