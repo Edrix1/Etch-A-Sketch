4617ee3b279b7de3b039a53f9ff7198108d25fd5
Edric <edhernal@yahoo.com>
2023-08-15 19:14:25 -0700

    added hover event

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const container = document.querySelector('.grid-container');
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i <256; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
     container.appendChild(gridSquare);
 }
+
+const gridSquare = document.querySelectorAll('.grid-square');
+
+gridSquare.forEach (gridSquare => (
+    gridSquare.addEventListener('mouseenter', () => {
+        gridSquare.classList.add('hover-square');
+    })
+))
